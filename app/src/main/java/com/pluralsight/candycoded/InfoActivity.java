8a4dc6bdd56ab77b4aac03025b05ad4e34124684
EleonorSemevo <eleonorsemevo@gmail.com>
2020-10-12 10:25:11 +0100

    Task 3

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import static android.net.Uri.parse;
+
 public class InfoActivity extends AppCompatActivity {
 
     @Override
@@ -16,7 +18,7 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
-        Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
+        Uri uri = parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
         ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
         Picasso.with(this).
                 load(uri).
@@ -29,7 +31,7 @@ public class InfoActivity extends AppCompatActivity {
     // TODO - Task 2 - Launch the Google Maps Activity
     public void createMapIntent(View view)
     {
-        Uri uri =Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Uri uri = parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent =new Intent(Intent.ACTION_VIEW,uri);
         mapIntent.setPackage("com.google.android.apps.maps");
         if(mapIntent.resolveActivity(getPackageManager())!=null)
@@ -41,5 +43,13 @@ public class InfoActivity extends AppCompatActivity {
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
+    public void createPhoneIntent(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        Uri uri =Uri.parse("tel:0123456789");
+        intent.setData(uri);
+        startActivity(intent);
+
+    }
     // ***
 }
